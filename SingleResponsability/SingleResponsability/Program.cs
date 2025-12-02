444df7d03314d507f61c4a9db0c06106f9ba6b20
Ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2025-12-02 10:23:28 +0500

    Ajoutez des fichiers projet.

---
+++ SingleResponsability/SingleResponsability/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using SingleResponsability.Models;
+
+var j = new Journal();
+j.AddEntry("I cried today.");
+j.AddEntry("I ate a bug.");
+Console.WriteLine(j);
+
+var p = new Persistence();
+var filename = @"F:\Projet\Design Patterns in C#\journal.txt";
+p.SaveToFile(j, filename, true);
+var psi = new ProcessStartInfo
+{
+    FileName = filename,
+    UseShellExecute = true
+};
+Process.Start(psi);
+
+// A typical class is responsible for on thing and has one reason to change.
