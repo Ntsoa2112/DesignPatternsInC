cb05cce73f76ccd27be10fe10639c762825f55ad
Ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2025-12-11 09:56:49 +0500

    Init Liskov Substitution Principle

---
+++ LiskovSubstitutionPrinciple/LiskovSubstitutionPrinciple/Models/Util.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciple.Models
+{
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle()
+        {
+            
+        }
+
+        public Rectangle(int width, int height)
+        {
+            width = Width;
+            height = Height;
+        }
+         
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+}
