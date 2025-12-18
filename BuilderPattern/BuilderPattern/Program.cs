2a7b9d51f844420cf5f3c55c3e1f03b7ca41752e
Ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2025-12-18 10:10:02 +0500

    Init Builder pattern

---
+++ BuilderPattern/BuilderPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿var hello = "Hello";
+var sb = new System.Text.StringBuilder();
+sb.Append("<p>");
+sb.Append(hello);
+sb.Append("</p>");
+System.Console.WriteLine(sb.ToString());
+
+var words = new[] { "Hello", "from", "C#", "12" };
+sb.Clear();
+sb.Append("<ul>");
+foreach (var word in words)
+{
+    sb.AppendFormat("<li>{0}</li>", word);
+}
+sb.Append("</ul>");
+System.Console.WriteLine(sb.ToString());
